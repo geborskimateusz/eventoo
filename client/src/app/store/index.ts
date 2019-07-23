06038be987c608106f999fe7185311399213a526
carrothead <Loczek11>
2019-07-23 16:29:16 +0200

    added ngrx store freeze

--- client/src/app/store/index.ts
+++ client/src/app/store/index.ts
@@ -6,6 +6,8 @@ import {
   MetaReducer
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
+import { storeFreeze } from 'ngrx-store-freeze';
+
 
 export interface AppState {
 
@@ -15,4 +17,4 @@ export const reducers: ActionReducerMap<AppState> = {
 };
 
 
-export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [storeFreeze] : [];
