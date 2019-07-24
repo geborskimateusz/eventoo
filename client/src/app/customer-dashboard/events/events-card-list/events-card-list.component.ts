22c53a319c99cd04a29e063a8b2da57999fdf310
carrothead <Loczek11>
2019-07-24 16:56:35 +0200

    working on dynamic mat tabs

--- client/src/app/customer-dashboard/events/events-card-list/events-card-list.component.ts
+++ client/src/app/customer-dashboard/events/events-card-list/events-card-list.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {Event} from '../../../shared/event.model';
+import { Event } from '../../../shared/event.model';
+import { Observable, of } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store';
+import { EVENTS_DATASOURCE } from 'src/app/shared/events-datasource';
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-events-card-list',
   templateUrl: './events-card-list.component.html',
@@ -7,14 +12,25 @@ import {Event} from '../../../shared/event.model';
 })
 export class EventsCardListComponent implements OnInit {
 
-  @Input() events: Event[];
+  @Input() musicGenre: string;
+  
+  events$: Observable<Event[]>;
 
-  constructor() { }
+
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
-    console.log(this.events)
+    console.log(this.musicGenre)
+       // const events$ = this.store.pipe(
+    //   select(selectAllEvents)
+    // );
+    // const events$ = of(EVENTS_DATASOURCE);
+
+    //   this.events$ = events$.pipe(
+    //   map(events => events.filter(event => event.genre === this.musicGenre.toUpperCase()))
+    // )
   }
-  
+
   getDate(event: Event) {
     // let event = this.events$[index];
     return `${event.date.getMonth()}, ${event.date.getDay()}`;
