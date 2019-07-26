dea32370e4db380b883bd4ef8f17af90b4298e1a
carrothead <Loczek11>
2019-07-26 07:24:21 +0200

    added ticket component, refactored Ticket model

--- client/src/app/customer-dashboard/book/book.component.ts
+++ client/src/app/customer-dashboard/book/book.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Event } from 'src/app/shared/event.model';
 import { ActivatedRoute } from '@angular/router';
 
@@ -16,23 +16,19 @@ export class BookComponent implements OnInit {
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
 
-  constructor(private formBuilder: FormBuilder, private router: ActivatedRoute) { }
+  avilableTickets = [];
+  userShoppingList = [];
+
+  constructor(private formBuilder: FormBuilder,
+    private router: ActivatedRoute) { }
 
   ngOnInit() {
     this.event = this.router.snapshot.data['event'];
 
-    const tickets = this.event.tickets;
-    console.log(tickets)
-    this.avilableTickets = [tickets.VIP, tickets.generalAdmission, tickets.goldenCircleEarlyEntrance, tickets.goldenCircleRegular, tickets.stands]
+    this.initAvilableTickets();
   }
 
-  avilableTickets = [
-   
-  ];
 
-  userShoppingList = [
-  
-  ];
 
   drop(event: CdkDragDrop<string[]>) {
 
@@ -40,10 +36,17 @@ export class BookComponent implements OnInit {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
       transferArrayItem(event.previousContainer.data,
-                        event.container.data,
-                        event.previousIndex,
-                        event.currentIndex);
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
     }
   }
 
+  private initAvilableTickets() {
+    // this.avilableTickets = this.event.tickets;
+  }
+
+  onShoppingListTicketAmmount() {
+
+  }
 }
---
+++ client/src/app/customer-dashboard/book/ticket/ticket.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket',
+  templateUrl: './ticket.component.html',
+  styleUrls: ['./ticket.component.css']
+})
+export class TicketComponent implements OnInit {
+
+  @Input()
+
+  constructor() { }
+
+  ngOnInit() {
+
+
+    for (const [type, ticketData] of Object.entries(tickets)) {
+      this.avilableTickets.push({ name: type, ticket: ticketData });
+    }
+
+    console.log(this.avilableTickets)
+  }
+
+}
