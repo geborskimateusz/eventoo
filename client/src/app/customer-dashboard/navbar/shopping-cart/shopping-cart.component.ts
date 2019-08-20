7553e3e32f3357c13d877d59d7e4c41f721fdc6c
carrothead <Loczek11>
2019-08-20 20:19:45 +0200

    added shopping cart navbar functionality

--- client/src/app/customer-dashboard/navbar/shopping-cart/shopping-cart.component.ts
+++ client/src/app/customer-dashboard/navbar/shopping-cart/shopping-cart.component.ts
@@ -1,30 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { AppState } from 'src/app/store';
 import { Store, select } from '@ngrx/store';
-import { isShoppingCartEmpty, selectTotalCount } from './store/shopping-cart.selectors';
-import { tap } from 'rxjs/operators';
-
+import { isShoppingCartEmpty, selectTotalCount, selectEventsFromShoppingList } from './store/shopping-cart.selectors';
+import { tap, map } from 'rxjs/operators';
+import { Event } from "/home/mat/Projects/eventoo-app/client/src/app/shared/model/event.model";
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html',
-  styleUrls: ['./shopping-cart.component.scss']
+  styleUrls: ['./shopping-cart.component.scss'],
 })
 export class ShoppingCartComponent implements OnInit {
 
   isShoppingCartEmpty$: Observable<boolean>;
   count$: Observable<number>;
+  shoppingCartFragment$: Observable<Event[]>
+
 
   constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
     this.isShoppingCartEmpty$ = this.store.pipe(
-      select(isShoppingCartEmpty)
+      select(isShoppingCartEmpty),
+      tap(flad => console.log(flad))
     );
 
     this.count$ = this.store.pipe(
       select(selectTotalCount),
     )
+
+    this.shoppingCartFragment$ = this.store.pipe(
+      select(selectEventsFromShoppingList),
+      map(events => this.paginateEvents(events))
+    )
+  }
+
+  private paginateEvents(events: Event[]): Event[] {
+
+    let length = events.length;
+
+    if (length <= 5) {
+      return events;
+    } else {
+      return events.slice(length - 5, length);
+    }
   }
 
 }
