9eb725afc7aacbf1160adbd1e8c3a3600065759c
carrothead <Loczek11>
2019-08-20 15:51:14 +0200

    added shopping cart counter to navbar

--- client/src/app/customer-dashboard/navbar/shopping-cart/shopping-cart.component.ts
+++ client/src/app/customer-dashboard/navbar/shopping-cart/shopping-cart.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { AppState } from 'src/app/store';
 import { Store, select } from '@ngrx/store';
-import { isShoppingCartEmpty } from './store/shopping-cart.selectors';
+import { isShoppingCartEmpty, selectTotalCount } from './store/shopping-cart.selectors';
 import { tap } from 'rxjs/operators';
 
 @Component({
@@ -12,14 +12,18 @@ import { tap } from 'rxjs/operators';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  isShoppingCartEmpty: Observable<boolean>;
+  isShoppingCartEmpty$: Observable<boolean>;
+  count$: Observable<number>;
 
   constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
-    this.isShoppingCartEmpty = this.store.pipe(
-      select(isShoppingCartEmpty),
-      tap(flag => console.log(flag))
+    this.isShoppingCartEmpty$ = this.store.pipe(
+      select(isShoppingCartEmpty)
+    );
+
+    this.count$ = this.store.pipe(
+      select(selectTotalCount),
     )
   }
 
