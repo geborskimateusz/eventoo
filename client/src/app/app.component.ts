54c43e6334963ece4bc052ab1ba7f4ab306ac3d6
carrothead <Loczek11>
2019-09-19 09:44:49 +0200

    added on update service worker confirmation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { OnIdentifyEffects } from '@ngrx/effects';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'eventoo';
+
+  constructor(private swUpdate: SwUpdate) { }
+
+  ngOnInit() {
+    this.onServiceWorkerUpdate();
+  }
+
+  onServiceWorkerUpdate() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('New Version abilable, Would you like to load it?')) {
+          window.location.reload();
+        }
+      })
+    }
+  }
 }
